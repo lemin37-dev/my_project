005c23484cd0e6a267d3fe7ed37e679b13e35740
lemin37-dev <lemin37.dev@gmail.com>
2026-07-22 13:38:10 +0900

    Feat: 홈페이지 구성

---
+++ main.py
@@ -0,0 +1,17 @@
+# 간단한 홈페이지 제공
+# 1. 모듈 가져오기
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+# 2. fastapi 객체 생성
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+# 3. 라우팅 구성
+@app.get("/")
+def home(req:Request):
+    # 홈페이지 요청(GET)
+    # > index.html을 읽어 req 데이터 전달하여 동적 html 구성
+    # > 응답
+    # > 브라우저 화면 구성
+    return templates.TemplateResponse("index.html", {"req":req})
